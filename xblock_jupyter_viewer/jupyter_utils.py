8c47318395317863f252ff5ab61422e151183c42
idragon <isehrob@gmail.com>
2020-08-02 18:03:00 +0500

    python3 compatability fix

--- xblock_jupyter_viewer/jupyter_utils.py
+++ xblock_jupyter_viewer/jupyter_utils.py
@@ -6,8 +6,8 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 from nbconvert import HTMLExporter
 
-from preprocessors import ImageReplacement, RemoveCustomCSS
-from post_processors import remove_box_shadow, insert_target_blank
+from xblock_jupyter_viewer.preprocessors import ImageReplacement, RemoveCustomCSS
+from xblock_jupyter_viewer.post_processors import remove_box_shadow, insert_target_blank
 
 
 log = logging.getLogger(__name__)
